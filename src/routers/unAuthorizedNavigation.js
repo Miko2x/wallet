df8a513439e99107d6950783f5dae3c11b91f9b9
Miko2x <miko.ikhwan20@gmail.com>
2019-01-08 12:53:14 +0700

    update unAuthorized

--- src/routers/unAuthorizedNavigation.js
+++ src/routers/unAuthorizedNavigation.js
@@ -1,11 +1,18 @@
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator } from "react-navigation";
 
-import Login from '../screens/login/Login.container';
-import SignUp from '../screens/signup/Signup.container';
+import Login from "../screens/login/Login.container";
+import SignUp from "../screens/signup/Signup.container";
+import Forgot from "../screens/login/ForgotPassword";
 
 const UnAuthNavigation = createStackNavigator({
     login: {
         screen: Login,
+        navigationOptions: {
+            header: null
+        }
+    },
+    forgot: {
+        screen: Forgot,
         navigationOptions: {
             header: null
         }
