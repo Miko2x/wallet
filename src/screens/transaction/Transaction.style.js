b3a05dbb485afac14829e0736a233ae9231def04
Miko2x <miko.ikhwan20@gmail.com>
2019-01-08 12:55:45 +0700

    Update transaction styles

--- src/screens/transaction/Transaction.style.js
+++ src/screens/transaction/Transaction.style.js
@@ -14,7 +14,7 @@ export default StyleSheet.create({
         width: "100%",
         alignItems: "center",
         justifyContent: "space-between",
-        paddingVertical: 20,
+        paddingVertical: 40,
     },
     subtitleView: {
         alignSelf: "center"
@@ -23,17 +23,29 @@ export default StyleSheet.create({
         fontSize: 40,
         fontWeight: "bold",
     },
-    textInputContainer: {
-        flexDirection: "column",
+    parentInputContainer: {
+        width: "60%",
+        alignItems: "center",
         justifyContent: "center",
     },
     textInputView: {
         width: "100%",
         alignSelf: "center",
+        justifyContent: "center"
+    },
+    chilContainer: { 
+        alignSelf: "center", 
+        flexDirection: "row" 
+    },
+    pickerView: {
+        width: "100%",
+        alignSelf: "center",
+        borderBottomWidth: 1,
+        borderBottomColor: "gray"
     },
     button: {
         flexDirection: "row",
-        alignItems: "center",
+        alignItems: 'center',
         justifyContent: "space-around"
     },
     buttonBlue: {
@@ -41,18 +53,18 @@ export default StyleSheet.create({
         height: 40,
         backgroundColor: "#42b9d6",
         elevation: 5,
+        borderRadius: 25,
         alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 25
+        justifyContent: 'center'
     },
     buttonRed: {
         width: "40%",
         height: 40,
         backgroundColor: "#ff5555",
         elevation: 5,
+        borderRadius: 25,
         alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 25
+        justifyContent: 'center'
     },
     dropDownView: {
         justifyContent: "center",
